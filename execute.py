15d8b768a6899f65a0c50892642e597bacf36709
Kisanet Molla <61582660+KisanetH@users.noreply.github.com>
2020-05-27 16:58:36 +0200

    Add files via upload

--- execute.py
+++ execute.py
@@ -1,12 +1,62 @@
 # WPS Execute Operation
 
-import requests, os
+import requests
+import os
+import json
+from urllib import parse
 
-payload = open(os.path.dirname(os.path.abspath(__file__)) +"\\envelope_calc.xml").read()
+wpsServerUrl = "http://130.89.221.193:85/geoserver/ows?"
+url="https://gisedu.itc.utwente.nl/cgi-bin/mapserv.exe?map=d:/iishome/exercise/data/afrialiance/layers.map&version=2.0.0&service=WFS&request=GetFeature&typeName=neighbourhood&outputFormat=geojson&srsname=EPSG:3857&buname"
+get_neighbourhoods = requests.get(url)
+w=get_neighbourhoods.text
 
-wpsServerUrl = "https://gisedu.itc.utwente.nl/student/s2257335/gpw/Envelope/wps.py?"
+neighbourhoods = json.loads(w)
+neighb=[]
+result = []
+for n in range(100):
+  neighb.append(neighbourhoods["features"][n]['properties']['bu_name'])
+for buname in neighb:
+  buname=parse.quote(buname)
+  payload = '''
+    <wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
+      <ows:Identifier>gs:Length</ows:Identifier>
+      <wps:DataInputs>
+        <wps:Input>
+          <ows:Identifier>feature</ows:Identifier>
+          <wps:Reference mimeType="text/xml" xlink:href="http://geoserver/wps" method="POST">
+            <wps:Body>
+              <wps:Execute version="1.0.0" service="WPS">
+                <ows:Identifier>gs:IntersectionFeatureCollection</ows:Identifier>
+                <wps:DataInputs>
+                  <wps:Input>
+                    <ows:Identifier>first feature collection</ows:Identifier>
+                    <wps:Reference mimeType="application/json" xlink:href="https://gisedu.itc.utwente.nl/cgi-bin/mapserv.exe?map=d:/iishome/exercise/data/afrialiance/layers.map&amp;version=2.0.0&amp;service=WFS&amp;request=GetFeature&amp;typeName=streets&amp;outputFormat=geojson&amp;srsname=EPSG:28992" method="GET"/>
+                  </wps:Input>
+                  <wps:Input>
+                    <ows:Identifier>second feature collection</ows:Identifier>
+                    <wps:Reference mimeType="application/json" xlink:href="https://gisedu.itc.utwente.nl/cgi-bin/mapserv.exe?map=d:/iishome/exercise/data/afrialiance/layers.map&amp;version=2.0.0&amp;service=WFS&amp;request=GetFeature&amp;typeName=neighbourhood&amp;outputFormat=geojson&amp;srsname=EPSG:28992&amp;buname=%s" method="GET"/>
+                  </wps:Input>
+                </wps:DataInputs>
+                <wps:ResponseForm>
+                  <wps:RawDataOutput mimeType="application/json">
+                    <ows:Identifier>result</ows:Identifier>
+                  </wps:RawDataOutput>
+                </wps:ResponseForm>
+              </wps:Execute>
+            </wps:Body>
+          </wps:Reference>
+        </wps:Input>
+      </wps:DataInputs>
+      <wps:ResponseForm>
+        <wps:RawDataOutput>
+          <ows:Identifier>result</ows:Identifier>
+        </wps:RawDataOutput>
+      </wps:ResponseForm>
+    </wps:Execute>
+    '''%(buname)
+  length = requests.post(wpsServerUrl, data=payload).text
+  result.append({'neighbourhood': buname, 'length': length})
 
-response = requests.post(wpsServerUrl, data=payload)
 print("Content-type: application/json")
 print()
-print(response.text)
+print(result)
